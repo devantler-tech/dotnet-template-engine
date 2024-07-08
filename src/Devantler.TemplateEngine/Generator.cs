7bfd0b9a642d5adecff4407ffadb89d9e102ac4d
Nikolai Emil Damm <ned@devantler.com>
2024-07-08 20:41:25 +0200

    Add TemplateEngine (#2)
    * Add TemplateEngine
    
    * chore: Add .editorconfig file and update project settings
    
    * chore: Remove CONTRIBUTING.md file
    
    * chore: Update workflow_dispatch in test.yaml

--- src/Devantler.TemplateEngine/Generator.cs
+++ src/Devantler.TemplateEngine/Generator.cs
@@ -1,31 +1,31 @@
 using System.Text;
 
-namespace Devantler.Commons.TemplateEngine;
+namespace Devantler.TemplateEngine;
 
 /// <inheritdoc />
 public class Generator(ITemplateEngine templateEngine) : IGenerator
 {
-  readonly ITemplateEngine _templateEngine = templateEngine;
+    readonly ITemplateEngine _templateEngine = templateEngine;
 
-  /// <inheritdoc />
-  public Task<string> GenerateAsync(string templatePath, object model) =>
-    _templateEngine.RenderFromPathAsync(templatePath, model);
+    /// <inheritdoc />
+    public Task<string> GenerateAsync(string templatePath, object model) =>
+      _templateEngine.RenderFromPathAsync(templatePath, model);
 
-  /// <inheritdoc />
-  public async Task GenerateAsync(
-    string outputPath,
-    string templatePath,
-    object model,
-    FileMode fileMode = FileMode.CreateNew
-  )
-  {
-    string directoryName = Path.GetDirectoryName(outputPath) ?? throw new ArgumentNullException(nameof(outputPath));
-    if (!Directory.Exists(directoryName))
-      _ = Directory.CreateDirectory(directoryName);
+    /// <inheritdoc />
+    public async Task GenerateAsync(
+      string outputPath,
+      string templatePath,
+      object model,
+      FileMode fileMode = FileMode.CreateNew
+    )
+    {
+        string directoryName = Path.GetDirectoryName(outputPath) ?? throw new ArgumentNullException(nameof(outputPath));
+        if (!Directory.Exists(directoryName))
+            _ = Directory.CreateDirectory(directoryName);
 
-    var fileStream = new FileStream(outputPath, fileMode, FileAccess.Write);
-    await fileStream.WriteAsync(Encoding.UTF8.GetBytes(await _templateEngine.RenderFromPathAsync(templatePath, model)));
-    await fileStream.FlushAsync();
-    fileStream.Close();
-  }
+        var fileStream = new FileStream(outputPath, fileMode, FileAccess.Write);
+        await fileStream.WriteAsync(Encoding.UTF8.GetBytes(await _templateEngine.RenderFromPathAsync(templatePath, model)));
+        await fileStream.FlushAsync();
+        fileStream.Close();
+    }
 }
