45989a70e85f59a7ae40548c8d254df173678dd1
Nikolai Emil Damm <ned@devantler.com>
2024-07-08 21:28:44 +0200

    Update indentation

--- src/Devantler.TemplateEngine/IGenerator.cs
+++ src/Devantler.TemplateEngine/IGenerator.cs
@@ -5,21 +5,21 @@ namespace Devantler.TemplateEngine;
 /// </summary>
 public interface IGenerator
 {
-    /// <summary>
-    /// Generates a file asynchronously based on a template and a model.
-    /// </summary>
-    /// <param name="outputPath">The path where the generated file will be saved.</param>
-    /// <param name="templatePath">The path to the template file.</param>
-    /// <param name="model">The model object used for generating the file.</param>
-    /// <param name="fileMode">The file mode used when creating the file. Default is FileMode.CreateNew.</param>
-    /// <returns>A task representing the asynchronous operation.</returns>
-    Task GenerateAsync(string outputPath, string templatePath, object model, FileMode fileMode = FileMode.CreateNew);
+  /// <summary>
+  /// Generates a file asynchronously based on a template and a model.
+  /// </summary>
+  /// <param name="outputPath">The path where the generated file will be saved.</param>
+  /// <param name="templatePath">The path to the template file.</param>
+  /// <param name="model">The model object used for generating the file.</param>
+  /// <param name="fileMode">The file mode used when creating the file. Default is FileMode.CreateNew.</param>
+  /// <returns>A task representing the asynchronous operation.</returns>
+  Task GenerateAsync(string outputPath, string templatePath, object model, FileMode fileMode = FileMode.CreateNew);
 
-    /// <summary>
-    /// Generates a string output based on the specified template and model asynchronously.
-    /// </summary>
-    /// <param name="templatePath">The path to the template file.</param>
-    /// <param name="model">The model object used to populate the template.</param>
-    /// <returns>A task representing the asynchronous operation. The task result contains the generated string output.</returns>
-    Task<string> GenerateAsync(string templatePath, object model);
+  /// <summary>
+  /// Generates a string output based on the specified template and model asynchronously.
+  /// </summary>
+  /// <param name="templatePath">The path to the template file.</param>
+  /// <param name="model">The model object used to populate the template.</param>
+  /// <returns>A task representing the asynchronous operation. The task result contains the generated string output.</returns>
+  Task<string> GenerateAsync(string templatePath, object model);
 }
